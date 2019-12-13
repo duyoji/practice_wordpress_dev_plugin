34707efeadc2807baefcf32fa46f2d2839598897
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-12-13 18:58:29 +0900

    Added module by composer and add scripts.

--- wordpress/wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wordpress/wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -1,9 +1,11 @@
 <?php
-/*
-Plugin Name: My First Plugin
-Author: Tsuyoshi Maeda
-Author URI: https://tsuyopon.xyz/
-Plugin URI: https://tsuyopon.xyz/
-Description: 練習です
-Version: 1.0.0
-*/
+/**
+ * Plugin Name: My First Plugin
+ * Author: Tsuyoshi Maeda
+ * Author URI: https://tsuyopon.xyz/
+ * Plugin URI: https://tsuyopon.xyz/
+ * Description: 練習です
+ * Version: 1.0.0
+ */
+
+$hello = "a";
