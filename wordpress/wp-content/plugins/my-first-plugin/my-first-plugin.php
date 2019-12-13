80995b6548214119b386187016c44b51ccb19269
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-12-13 19:15:30 +0900

    Added action and filter into my-first-plugin.php

--- wordpress/wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wordpress/wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -8,4 +8,14 @@
  * Version: 1.0.0
  */
 
-$hello = "a";
+
+add_filter('the_content', 'insert_db_to_the_content');
+function insert_db_to_the_content($content) {
+  $html = '<div class="my_ad_space_style">ここにバナー広告</div>';
+  return $html . $content;
+}
+
+add_action('wp_head', 'my_ad_space_style');
+function my_ad_space_style() {
+  echo '<style>.my_ad_space_style{ margin: 1em 0; }</style>';
+}
